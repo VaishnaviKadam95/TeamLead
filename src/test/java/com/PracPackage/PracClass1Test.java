b54a762c1b0e9ec2d3658d26c8c581ceece56e7e
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2023-12-01 15:39:35 +0530

    FrameWork is done

---
+++ src/test/java/com/PracPackage/PracClass1Test.java
@@ -0,0 +1,10 @@
+package com.PracPackage;
+
+public class PracClass1Test {
+public void test1() {
+	System.out.println("===Test1===");
+}
+public void test2() {
+	System.out.println("===Test2===");
+}
+}
---
+++ src/test/java/com/PracPackage/PracClass2Test.java
@@ -0,0 +1,10 @@
+package com.PracPackage;
+
+public class PracClass2Test {
+	public void samptest1() {
+		System.out.println("===SampTest1===");
+	}
+	public void samptest2() {
+		System.out.println("===SampTest2===");
+	}
+}
