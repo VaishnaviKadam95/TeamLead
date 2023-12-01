46ec8a802911cb654fd80a649613f9cb2fdacf6c
prasaddesai95 <prasaddesai811@gmail.com>
2023-12-01 15:47:08 +0530

    Prasad Commit

---
+++ src/test/java/com/PrasadEngg/practice234Test.java
@@ -0,0 +1,9 @@
+package com.PrasadEngg;
+
+public class practice234Test {
+
+	public static void main(String[] args) {
+		System.out.println("chair tutel");
+		System.out.println("todnar ahes ka chari?");
+	}
+}
